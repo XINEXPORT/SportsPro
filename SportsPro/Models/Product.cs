80d67ff6ab5f4f6c70d2165ee0d19b350342881b
Christine Hoang <christinehoangt@gmail.com>
2024-09-28 21:52:44 -0500

    4-1 finished

--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsPro.Models
 {
@@ -8,7 +9,9 @@ namespace SportsPro.Models
 		public string ProductCode { get; set; } = string.Empty;
 		public string Name { get; set; } = string.Empty;
 		[Column(TypeName = "decimal(8,2)")]
-		public decimal YearlyPrice { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Yearly Price must be greater than zero.")]
+        public decimal YearlyPrice { get; set; }
 		public DateTime ReleaseDate { get; set; } = DateTime.Now;
 	}
 }
