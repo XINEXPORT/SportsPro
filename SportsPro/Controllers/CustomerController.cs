0de8ea8bd3e540140b0164bd4daf2ca7a6e8e5d6
Christine Hoang <christinehoangt@gmail.com>
2024-10-02 20:26:04 -0500

    Incident Controller walk thru with Will.

---
+++ SportsPro/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly SportsProContext _context;
+
+        // Constructor
+        public CustomerController(SportsProContext context)
+        {
+            _context = context;
+        }
+
+        // GET THE CUSTOMER LIST
+        public IActionResult List()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        // GET THE ADD CUSTOMER VIEW
+        public IActionResult Add()
+        {
+            return View(new Customer());
+        }
+
+        // POST -  ADD A CUSTOMER
+        [HttpPost]
+        public IActionResult Add(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            return View(customer);
+        }
+
+        // GET THE EDIT CUSTOMER VIEW
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST - ADD THE EDITED CUSTOMER
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Update(customer);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            return View(customer);
+        }
+
+        // GET THE DELETE CUSTOMER VIEW
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST - DELETE THE CUSTOMER
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ SportsPro/Controllers/IncidentController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsPro.Controllers
+{
+    public class IncidentController : Controller
+    {
+        private SportsProContext context { get; set; }
+
+        public IncidentController(SportsProContext ctx) => context = ctx;
+
+        public IActionResult Index() => RedirectToAction("List");
+        // GET THE INCIDENT LIST
+        public IActionResult List()
+        {
+            List<Incident> incidents = context.Incidents
+                .Include(i => i.Customer)
+                .Include(i => i.Product)
+                .OrderBy(i => i.DateOpened)
+                .ToList();
+            return View(incidents);
+        }
+
+        // GET THE ADD INCIDENT VIEW
+        public void StoreDataInViewBag(string action)
+        {
+            //this a dynamic object that allows u to pass data from the controller to the view
+            ViewBag.Action = action;
+            ViewBag.Customers = context.Customers
+                .OrderBy(c => c.FirstName)
+                .ToList();
+            ViewBag.Products = context.Products
+                .OrderBy(c => c.Name)
+                .ToList();
+            ViewBag.Technicians = context.Technicians
+                .OrderBy(c => c.Name)
+                .ToList();
+        }
+
+        // GET ADD - NEW INCIDENT
+        [HttpGet]
+        public IActionResult Add()
+        {
+            StoreDataInViewBag("Add");
+            return View("AddEdit", new Incident());
+        }
+        
+        //GET EDIT - FETCH THE INCIDENT ID FOR EDITING
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            StoreDataInViewBag("Edit");
+            var product = context.Incidents.Find(id);
+            return View("AddEdit");
+        }
+        //POST & SAVE
+        [HttpPost]
+
+        public IActionResult Save(Incident incident) 
+        {
+            if (ModelState.IsValid)
+            {
+                if (incident.IncidentID == 0)
+                {
+                    context.Incidents.Add(incident);
+                }
+                else
+                {
+                    context.Incidents.Update(incident);
+                }
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                if (incident.IncidentID == 0)
+                {
+                    StoreDataInViewBag("Add");
+                }
+                else 
+                {
+                    StoreDataInViewBag("Edit");
+                }
+                return View("AddEdit", incident);
+            }
+        }
+
+        // GET DELETE RECORD
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Incidents.Find(id);
+            return View(product);
+        }
+
+        //POST DELETED RECORD
+        [HttpPost]
+        public IActionResult Delete(Incident incident)
+        {
+            context.Incidents.Remove(incident);
+            context.SaveChanges();
+            return RedirectToAction("List");
+            
+        }
+
+
+
+
+    
+
+    }
+}
