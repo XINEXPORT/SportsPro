336cccbb9a73f2f6bdb3ae03c904aab2e2f46b86
Christine Hoang <christinehoangt@gmail.com>
2024-10-21 10:07:35 -0500

    AddIncidentView model added

---
+++ SportsPro/Models/AddEditIncidentView.cs
@@ -0,0 +1,19 @@
+﻿using SportsPro.Models;
+using System.Collections.Generic;
+
+namespace SportsPro.ViewModels
+{
+    
+    public class AddEditInicidentView
+    { 
+    public List<Customer> Customers { get; set; }
+
+    public List<Product> Products { get; set; }
+
+    public List<Technician> Technicians { get; set; }
+
+    public Incident CurrentIncident { get; set; }
+
+    public string OperationMode { get; set; }
+}
+}
