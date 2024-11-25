c46013ab3ef75ec50b889cbf34f003a318c8d4f8
Christine Hoang <christinehoangt@gmail.com>
2024-11-24 19:13:38 -0600

    Migrations were ran and authorization is implemented in the Incidents View

---
+++ SportsPro/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+
+namespace SportsPro.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasData(
+                    new Product
+                    {
+                        ProductID = 1,
+                        ProductCode = "DRAFT10",
+                        Name = "Draft Manager 1.0",
+                        YearlyPrice = 4.99M,
+                        ReleaseDate = DateTime.Parse("2017-02-01"),
+                    },
+                    new Product
+                    {
+                        ProductID = 2,
+                        ProductCode = "DRAFT20",
+                        Name = "Draft Manager 2.0",
+                        YearlyPrice = 5.99M,
+                        ReleaseDate = DateTime.Parse("2019-07-15 00:00:00.000"),
+                    },
+                    new Product
+                    {
+                        ProductID = 3,
+                        ProductCode = "LEAG10",
+                        Name = "League Scheduler 1.0",
+                        YearlyPrice = 4.99M,
+                        ReleaseDate = DateTime.Parse("2016-05-01 00:00:00.000"),
+                    },
+                    new Product
+                    {
+                        ProductID = 4,
+                        ProductCode = "LEAGD10",
+                        Name = "League Scheduler Deluxe 1.0",
+                        YearlyPrice = 7.99M,
+                        ReleaseDate = DateTime.Parse("2016-08-01 00:00:00.000"),
+                    },
+                    new Product
+                    {
+                        ProductID = 5,
+                        ProductCode = "TEAM10",
+                        Name = "Team Manager 1.0",
+                        YearlyPrice = 4.99M,
+                        ReleaseDate = DateTime.Parse("2017-05-01 00:00:00.000"),
+                    },
+                    new Product
+                    {
+                        ProductID = 6,
+                        ProductCode = "TRNY10",
+                        Name = "Tournament Master 1.0",
+                        YearlyPrice = 4.99M,
+                        ReleaseDate = DateTime.Parse("2015-12-01 00:00:00.000"),
+                    },
+                    new Product
+                    {
+                        ProductID = 7,
+                        ProductCode = "TRNY20",
+                        Name = "Tournament Master 2.0",
+                        YearlyPrice = 5.99M,
+                        ReleaseDate = DateTime.Parse("2018-02-15 00:00:00.000"),
+                    }
+                );
+
+            modelBuilder
+                .Entity<Technician>()
+                .HasData(
+                    new Technician
+                    {
+                        TechnicianID = -1,
+                        Name = "Not assigned",
+                        Email = "",
+                        Phone = "",
+                    },
+                    new Technician
+                    {
+                        TechnicianID = 11,
+                        Name = "Alison Diaz",
+                        Email = "alison@sportsprosoftware.com",
+                        Phone = "800-555-0443",
+                    },
+                    new Technician
+                    {
+                        TechnicianID = 12,
+                        Name = "Jason Lee",
+                        Email = "jason@sportsprosoftware.com",
+                        Phone = "800-555-0444",
+                    },
+                    new Technician
+                    {
+                        TechnicianID = 13,
+                        Name = "Andrew Wilson",
+                        Email = "awilson@sportsprosoftware.com",
+                        Phone = "800-555-0449",
+                    },
+                    new Technician
+                    {
+                        TechnicianID = 14,
+                        Name = "Gunter Wendt",
+                        Email = "gunter@sportsprosoftware.com",
+                        Phone = "800-555-0400",
+                    },
+                    new Technician
+                    {
+                        TechnicianID = 15,
+                        Name = "Gina Fiori",
+                        Email = "gfiori@sportsprosoftware.com",
+                        Phone = "800-555-0459",
+                    }
+                );
+
+            modelBuilder
+                .Entity<Country>()
+                .HasData(
+                    new Country { CountryID = "AU", Name = "Australia" },
+                    new Country { CountryID = "AT", Name = "Austria" },
+                    new Country { CountryID = "BE", Name = "Belgium" },
+                    new Country { CountryID = "BR", Name = "Brazil" },
+                    new Country { CountryID = "CA", Name = "Canada" },
+                    new Country { CountryID = "CN", Name = "China" },
+                    new Country { CountryID = "DK", Name = "Denmark" },
+                    new Country { CountryID = "FI", Name = "Finland" },
+                    new Country { CountryID = "FR", Name = "France" },
+                    new Country { CountryID = "GR", Name = "Greece" },
+                    new Country { CountryID = "US", Name = "United States" },
+                    new Country { CountryID = "VN", Name = "Vietnam" }
+                );
+
+            modelBuilder
+                .Entity<Customer>()
+                .HasData(
+                    new Customer
+                    {
+                        CustomerID = 1002,
+                        FirstName = "Ania",
+                        LastName = "Irvin",
+                        Address = "PO Box 96621",
+                        City = "Washington",
+                        State = "DC",
+                        PostalCode = "20090",
+                        CountryID = "US",
+                        Phone = "(301) 555-8950",
+                        Email = "ania@mma.nidc.com",
+                    },
+                    new Customer
+                    {
+                        CustomerID = 1004,
+                        FirstName = "Kenzie",
+                        LastName = "Quinn",
+                        Address = "1990 Westwood Blvd Ste 260",
+                        City = "Los Angeles",
+                        State = "CA",
+                        PostalCode = "90025",
+                        CountryID = "US",
+                        Phone = "(800) 555-8725",
+                        Email = "kenzie@mma.jobtrak.com",
+                    }
+                );
+
+            modelBuilder
+                .Entity<Incident>()
+                .HasData(
+                    new Incident
+                    {
+                        IncidentID = 1,
+                        CustomerID = 1002,
+                        ProductID = 1,
+                        TechnicianID = 11,
+                        Title = "Could not install",
+                        Description = "Media appears to be bad.",
+                        DateOpened = DateTime.Parse("2020-01-08"),
+                        DateClosed = DateTime.Parse("2020-01-10"),
+                    },
+                    new Incident
+                    {
+                        IncidentID = 2,
+                        CustomerID = 1002,
+                        ProductID = 4,
+                        TechnicianID = 14,
+                        Title = "Error importing data",
+                        Description = "Received error message 415 while trying to import data from previous version.",
+                        DateOpened = DateTime.Parse("2020-01-09"),
+                        DateClosed = null,
+                    }
+                );
+        }
+    }
+}
---
+++ SportsPro/Migrations/20241125000751_InitialIdentitySetup.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsPro.Migrations
+{
+    public partial class InitialIdentitySetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "AE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "CH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "ES");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "GB");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "GL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "HK");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IN");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IS");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "IT");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "JP");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "LR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "MX");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "MY");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NG");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NL");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "NZ");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "PT");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "QA");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "SE");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "SG");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "TH");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "TR");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "UA");
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: "ZW");
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1006);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1008);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1012);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1010);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1015);
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 1,
+                column: "CustomerID",
+                value: 1002);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "CountryID", "Name" },
+                values: new object[,]
+                {
+                    { "AE", "United Arab Emirates" },
+                    { "CH", "Switzerland" },
+                    { "ES", "Spain" },
+                    { "GB", "United Kingdom" },
+                    { "GL", "Greenland" },
+                    { "HK", "Hong Kong" },
+                    { "IE", "Ireland" },
+                    { "IL", "Israel" },
+                    { "IN", "India" },
+                    { "IS", "Iceland" },
+                    { "IT", "Italy" },
+                    { "JP", "Japan" },
+                    { "LR", "Liberia" },
+                    { "MX", "Mexico" },
+                    { "MY", "Malaysia" },
+                    { "NG", "Nigeria" },
+                    { "NL", "Netherlands" },
+                    { "NZ", "New Zealand" },
+                    { "PH", "Philippines" },
+                    { "PR", "Puerto Rico" },
+                    { "PT", "Portugal" },
+                    { "QA", "Qatar" },
+                    { "SE", "Sweden" },
+                    { "SG", "Singapore" },
+                    { "TH", "Thailand" },
+                    { "TR", "Turkey" },
+                    { "UA", "Ukraine" },
+                    { "ZW", "Zimbabwe" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "Address", "City", "CountryID", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[,]
+                {
+                    { 1006, "3255 Ramos Cir", "Sacramento", "US", "amauro@yahoo.org", "Anton", "Mauro", "(916) 555-6670", "95827", "CA" },
+                    { 1008, "Box 52001", "San Francisco", "US", "kanthoni@pge.com", "Kaitlyn", "Anthoni", "(800) 555-6081", "94152", "CA" },
+                    { 1010, "PO Box 2069", "Fresno", "US", "kmayte@fresno.ca.gov", "Kendall", "Mayte", "(559) 555-9999", "93718", "CA" },
+                    { 1012, "4420 N. First Street, Suite 108", "Fresno", "US", "marvin@expedata.com", "Marvin", "Quintin", "(559) 555-9586", "93726", "CA" },
+                    { 1015, "27371 Valderas", "Mission Viejo", "US", "", "Gonzalo", "Keeton", "(214) 555-3647", "92691", "CA" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentID",
+                keyValue: 1,
+                column: "CustomerID",
+                value: 1010);
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentID", "CountryID", "CustomerID", "DateClosed", "DateOpened", "Description", "ProductID", "TechnicianID", "Title" },
+                values: new object[] { 3, null, 1015, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "Setup failed with code 104.", 6, 15, "Could not install" });
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentID", "CountryID", "CustomerID", "DateClosed", "DateOpened", "Description", "ProductID", "TechnicianID", "Title" },
+                values: new object[] { 4, null, 1010, null, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Program fails with error code 510, unable to open database.", 3, -1, "Error launching program" });
+        }
+    }
+}
